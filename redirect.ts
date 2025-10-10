e05ed294fd56810b55902e7d4389fa98c438fb73
yvonsguin-afk <yvonsguin@gmail.com>
2025-10-10 02:40:54 +0200

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "http//srv233619.hoster-test.ru/red"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://meet.jitsi.online/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
